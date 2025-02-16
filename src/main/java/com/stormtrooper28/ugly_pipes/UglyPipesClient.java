73ff274e8fe220c0fcb947e574172c34c6c291dc
stormbot28 <stormtrooper28@live.com>
2025-02-16 16:17:14 -0500

    Initial commit

---
+++ src/main/java/com/stormtrooper28/ugly_pipes/UglyPipesClient.java
@@ -0,0 +1,10 @@
+package com.stormtrooper28.ugly_pipes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UglyPipesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/stormtrooper28/ugly_pipes/UglyPipesDataGenerator.java
@@ -0,0 +1,11 @@
+package com.stormtrooper28.ugly_pipes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UglyPipesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
